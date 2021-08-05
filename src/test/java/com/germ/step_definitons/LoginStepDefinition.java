66f6b0c2dfa9e3a585536d64aa1049c3de9edb23
Klevis Muda <klevis.blue@gmail.com>
2021-08-05 20:47:21 +0200

    First commit

--- src/test/java/com/germ/step_definitons/LoginStepDefinition.java
+++ src/test/java/com/germ/step_definitons/LoginStepDefinition.java
@@ -1,6 +1,7 @@
 package com.germ.step_definitons;
 
 import com.germ.pages.LoginPage;
+import com.germ.utilities.BrowserUtils;
 import com.germ.utilities.ConfigurationReader;
 import com.germ.utilities.Driver;
 import io.cucumber.java.en.Then;
@@ -18,6 +19,7 @@ public class LoginStepDefinition {
     }
     @Then("user should not see {string} in text")
     public void user_should_not_see_in_text(String expected) {
+        BrowserUtils.waitForVisibility(loginPage.text, 20);
         String actual = loginPage.text.getText();
 
         Assert.assertTrue(actual.contains(expected));
